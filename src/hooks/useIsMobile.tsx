0867bde57c0d7bb396707122c736061a1c9de83c
Eddie <ngthuongdoan@gmail.com>
2022-10-22 21:04:24 +0700

    feat: add 404 page

---
+++ src/hooks/useIsMobile.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+
+export const useIsMobile = () => {
+	const [width, setWidth] = useState<number>(0)
+
+	function handleWindowSizeChange() {
+		setWidth(window?.innerWidth)
+	}
+
+	useEffect(() => {
+		window?.addEventListener("resize", handleWindowSizeChange)
+		return () => {
+			window?.removeEventListener("resize", handleWindowSizeChange)
+		}
+	}, [])
+
+	return { isMobile: width <= 768 }
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "../styles/index.scss"
 import type { AppProps } from "next/app"
+import AppWrapper from "components/AppWrapper"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<AppWrapper>
+			<Component {...pageProps} />
+		</AppWrapper>
+	)
 }
